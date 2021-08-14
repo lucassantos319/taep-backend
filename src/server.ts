d495c18cce037e5fd245496109a611e630eebb8f
lucas <lucas.sidnei32@gmail.com>
2021-08-14 15:24:50 -0300

    port problem

--- src/server.ts
+++ src/server.ts
@@ -19,4 +19,4 @@ app.use(express.json());
 app.use(userRoutes);
 app.use(projectRoutes);
 
-app.listen(3333,() => console.log("Server is running on port 3333"));
+app.listen(process.env.PORT || 5000,() => console.log("Server is running on port 3333"));
