7b0e61b4ce551a0d1de0df8c02b9455affe37112
Lucas Santos <lucas.sidnei32@gmail.com>
2021-10-07 15:30:16 -0300

    teste'

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -15,7 +15,7 @@ class UserController{
         try {
          
             const user = await userService.Create({first_name, last_name,email, password, user_type,nickname}); 
-            emailService.SendEmail(emails,texto,titulo);
+            emailService.SendEmail(emails,texto,titulo,1);
             
             return response.status(200).json({
                 login:true,
--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,23 +1,53 @@
 import nodemailer from "nodemailer";
 
 class EmailService
-{
-    SendEmail(emails: string[], texto: string, titulo: string){
-        var transport = {
-            host:'smtp.gmail.com',
-            auth:{
-                user:'taep.2021.server@gmail.com',
-                pass:'taep@2021'
+{    
+    SendEmail(emails: string[], texto: string, titulo: string,type:Number ){
+        
+        if ( emails.length > 0 ){
+            
+            var transport = {
+                host:'smtp.gmail.com',
+                auth:{
+                    user:'taep.2021.server@gmail.com',
+                    pass:'taep@2021'
+                }
             }
-        }
+    
+            var transporter = nodemailer.createTransport(transport);
+            transporter.verify((error,sucess) =>{
+                if ( error )
+                    console.log(error);
+                else
+                    console.log('congratz');
+            })
+           
+            // Welcome teacher message
+            if ( type == 1 )
+                Array(emails).forEach(email=>{
+                    transporter.sendMail({
+                        from:'"TAEP 4.0" <taep.2021.server@gmail.com>',
+                        to: email,
+                        subject:{titulo},
+                        html: '<div style="background-color: #162A52;padding-top:20px;padding-bottom:40px;font-family:Arial, Helvetica, sans-serif;text-align: center;"><div style=""><h1 style="color: white;">Bem vindo ao TAEP 4.0</h1></div><div style="margin-top:20px;"><p style="color:white">Seja bem vindo a plataforma do taep, estamos  </p></div></div>'
+                    });
+                    
+                });
 
-        var transporter = nodemailer.createTransport(transport);
-        transporter.verify((error,sucess) =>{
-            if ( error )
-                console.log(error);
-            else
-                console.log('congratz');
-        })
+            // Welcome student message
+            if ( type == 2 )
+                Array(emails).forEach(email=>{
+                    transporter.sendMail({
+                        from:'"TAEP 4.0" <taep.2021.server@gmail.com>',
+                        to: email,
+                        subject:{titulo},
+                        html: '<div style="background-color: #162A52;padding-top:20px;padding-bottom:40px;font-family:Arial, Helvetica, sans-serif;text-align: center;"><div style=""><h1 style="color: white;">Bem vindo ao TAEP 4.0</h1></div><div style="margin-top:20px;"><p style="color:white">Seja bem vindo a plataforma do taep, estamos  </p></div></div>'
+                    });
+                    
+                });
+
+        }
+      
     }
 }
 
