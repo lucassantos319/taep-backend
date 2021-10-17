f18aa15a699a477e40ded12686a7ac25d2c3e6ff
Lucas Santos <lucas.sidnei32@gmail.com>
2021-10-17 20:01:45 -0300

    OK

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -44,7 +44,7 @@ class EmailService
                         from:'"TAEP 4.0" <taep.2021.server@gmail.com>',
                         to: email,
                         subject:{titulo},
-                        html:'<div style="background-color: #162A52;padding-top:20px;padding-bottom:40px;font-family:Arial, Helvetica, sans-serif;text-align: center;"><div style=""><h1 style="color: white;">Bem vindo ao TAEP 4.0</h1></div><div style="margin-top:20px;"><p style="color:white">Seja bem vindo a plataforma do TAEP, o professor '+professor+' fez a sua increção na plataforma.</p><p>Recomendamos que após o primeiro acesso seja realizado a troca da senha. Para seu primeiro acesso o login é o email que recebeu este email e a senha abaixo</p><div styl="padding:30px; background-color:white; border-radius:2px"><p style="font-size: 40px;">'+password+'</p></div></div></div>'
+                        html:'<div style=" background-color: #162A52;padding-top:20px;padding-bottom:70px;font-family:Arial, Helvetica, sans-serif;text-align: center;"><div><h1 style="color: white;">Bem vindo ao TAEP 4.0</h1></div><div style="margin-top:20px;"><p style="color:white">Seja bem vindo a plataforma do TAEP, o professor '+professor+' fez a sua inscrição na plataforma.</p><p style="color:white">Recomendamos que após o primeiro acesso seja realizado a troca da senha. Para seu primeiro acesso o login é o email que recebeu este email e a senha abaixo</p><div style=" margin-top:50px; margin-left:30%; width:40%; padding:5px; background-color:white; border-radius:30px;"><p style="font-size: 40px;">'+password+'</p></div></div></div>'
                     });
                     
                 });
